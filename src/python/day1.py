89092a3bb0e2711e1ed1ed8933011304bb9ff482
klauddius <klauddius@gmail.com>
2021-08-31 22:33:03 -0300

    Add the Sum of two numbers. (1/365)

---
+++ src/python/day1.py
@@ -0,0 +1,13 @@
+# Return the sum of Two Numbers
+
+def addition(var1, var2):
+    return var1 + var2
+
+
+
+print(addition(1, 2))
+print(addition(2.5, 1.3))
+print(addition(3, 2))
+print(addition(-3, -6))
+print(addition(7, 3))
+print(addition('1', '2'))
