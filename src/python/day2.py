9a8fb9f47f5491fe901aab99108b93130a4a3f41
klauddius <klauddius@gmail.com>
2021-09-01 23:22:15 -0300

    Adding Numbers from a String. (2/365)

---
+++ src/python/day2.py
@@ -0,0 +1,18 @@
+# Adding Numbers from a String
+# Based on https://edabit.com/challenge/stAFzKqQnWHztzrAW
+
+def add_nums(nums_in_string):
+    separator = ', '
+    list_nums = nums_in_string.split(separator)
+    sum = 0
+    for num in list_nums:
+        sum += float(num)
+
+    return sum
+
+# ➞ 20
+print(add_nums("2, 5, 1, 8, 4"))
+# ➞ 28
+print(add_nums("1, 2, 3, 4, 5, 6, 7"))
+# ➞ 10
+print(add_nums("10"))
