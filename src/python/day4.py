e9ddcc4ec3ee39ae2fe1c0eb3dbca9132f3be49b
klauddius <klauddius@gmail.com>
2021-09-03 21:57:16 -0300

    resolve #8 - How Many D's Are There? (4/365)

---
+++ src/python/day4.py
@@ -0,0 +1,16 @@
+# Day 4 - How Many D's Are There?
+# Based on https://edabit.com/challenge/xdSKkXQkkMroNzq8C
+
+def count_d(str_text):
+    count = 0
+    for t in str_text:
+        if t.lower() == 'd':
+            count += 1
+    return count
+
+# ➞ 4
+print(count_d("My friend Dylan got distracted in school."))
+# ➞ 3
+print(count_d("Debris was scattered all over the yard."))
+# ➞ 3
+print(count_d("The rodents hibernated in their den."))
