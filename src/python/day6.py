d23b13f19f8d94831cad84b9f3e8db1cbfe28684
klauddius <klauddius@gmail.com>
2021-09-07 23:03:42 -0300

    resolve #10: Numbers to Arrays and Vice Versa (6/365)

---
+++ src/python/day6.py
@@ -0,0 +1,44 @@
+# Numbers to Arrays and Vice Versa
+# Based on: https://edabit.com/challenge/L9nvCobbYyGgKpWi8
+
+def to_list(num_arg):
+    """Converts a number to an integer list of its digits."""
+    integer_lst = list()
+    num_tmp = abs(num_arg)
+
+    while num_tmp // 10 > 0:
+        integer_lst.insert(0, num_tmp % 10)
+        num_tmp = num_tmp // 10
+
+    integer_lst.insert(0, num_tmp % 10)
+
+    if num_arg < 0:
+        integer_lst.insert(0, '-')
+
+    return integer_lst
+
+
+def to_number(integer_lst):
+    if len(integer_lst) == 0:
+        raise ValueError
+
+    multiplier = 1
+    num = 0
+
+    for digit in reversed(integer_lst):
+        if digit == '-':
+            num *= -1
+            continue
+
+        num += digit * multiplier
+        multiplier *= 10
+
+    return num
+
+
+print(to_list(-9658325))
+print(to_list(456789))
+
+print(to_number([1,2,3]))
+print(to_number([4,5,6]))
+print(to_number(['-', 1, 2, 3]))
---
+++ src/tests/test_day6.py
@@ -0,0 +1,41 @@
+import pytest
+import day6
+
+
+def test_to_list_number_zero():
+    assert day6.to_list(0) == [0]
+
+
+def test_to_list_negative_number():
+    assert day6.to_list(-123) == ['-', 1, 2, 3]
+
+
+def test_to_list_positive_number():
+    assert day6.to_list(456) == [4, 5, 6]
+
+
+def test_to_list_wrong_arg():
+    with pytest.raises(TypeError):
+        day6.to_list('test')
+
+
+def test_to_number_empty_list():
+    with pytest.raises(ValueError):
+        day6.to_number(list())
+
+
+def test_to_number_list_with_invalid_items():
+    with pytest.raises(TypeError):
+        day6.to_number(['t', 'e', 's', 't'])
+
+
+def test_to_number_list_number_zero():
+    assert day6.to_number([0, ]) == 0
+
+
+def test_to_number_list_negative_number():
+    assert day6.to_number(['-', 1, 2, 3]) == -123
+
+
+def test_to_number_list_positive_number():
+    assert day6.to_number([4, 5, 6]) == 456
