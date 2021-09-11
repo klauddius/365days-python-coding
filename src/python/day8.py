c4a7f15386083ae84d7baeb07e0ac2f570cbba71
klauddius <klauddius@gmail.com>
2021-09-10 23:20:34 -0300

    resolve #16: Convert Minutes into Seconds. (8/365)

---
+++ src/python/day8.py
@@ -0,0 +1,15 @@
+# Convert Minutes into Seconds
+# Based on https://edabit.com/challenge/FQyaaJx7orS7tiwz8
+
+def convert(minutes: int):
+    return minutes * 60
+
+
+# 300
+print(convert(5))
+
+# 180
+print(convert(3))
+
+# 120
+print(convert(2))
---
+++ src/tests/test_day8.py
@@ -0,0 +1,15 @@
+import pytest
+import day8
+
+
+def test_convert_minutes_zero():
+    assert day8.convert(0) == 0
+
+
+def test_convert_minute_one():
+    assert day8.convert(1) == 60
+
+
+def test_convert_negative_minutes():
+    assert day8.convert(-3) == -180
+
