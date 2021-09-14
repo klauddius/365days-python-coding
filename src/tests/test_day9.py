3151d2ca3883f2f593fe40762e3d620d8d8e7c29
klauddius <klauddius@gmail.com>
2021-09-13 23:06:58 -0300

    resolve #17: Maximum Edge of a Triangle. (9/365)

--- src/tests/test_day9.py
+++ src/tests/test_day9.py
@@ -18,3 +18,24 @@ def test_next_edge_negative_integers_wrong_args():
     with pytest.raises(ValueError):
         day9.next_edge(-1, -1)
 
+
+
+def test_next_edge_success_example4():
+    assert day9.next_edge(5, 4) == 8
+
+
+def test_next_edge_success_example5():
+    assert day9.next_edge(8, 3) == 10
+
+
+def test_next_edge_success_example6():
+    assert day9.next_edge(7, 9) == 15
+
+
+def test_next_edge_success_example7():
+    assert day9.next_edge(10, 4) == 13
+
+
+def test_next_edge_success_example8():
+    assert day9.next_edge(7, 2) == 8
+
